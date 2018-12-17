58819b64ddcf1e46d8e5fd37377e8a4b0629a0fe
Robert McCary <robert.mccary@yahoo.com>
2018-12-17 21:02:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,24 +1,11 @@
 def nyc_pigeon_organizer(data)
-  new_hash = {}
-data.each do |attribute, descriptions|
-  descriptions.each do |description, pigeons|
-    pigeons.each do |pigeon|
-      new_hash[pigeon] ||= {}
-      new_hash[pigeon][attribute] ||= []
-      new_hash[pigeon][attribute] << option.to_s
+  data.each_with_object({}) do |(key, attributes), new_hash|
+    attributes.each do |value, names|
+      names.each do |name|
+        result[name] ||= {}
+        result[name][key] ||= []
+        result[name][key] << value.to_s
+      end
     end
   end
 end
-
-end
-# def nyc_pigeon_organizer(data)
-#   data.each_with_object({}) do |(key, h), result|
-#     h.each do |value, names|
-#       names.each do |name|
-#         result[name] ||= {}
-#         result[name][key] ||= []
-#         result[name][key] << value.to_s
-#       end
-#     end
-#   end
-# end
