77ad7a7ac3df7abbe703a4fd3877f0e76c0dd3fe
Robert McCary <robert.mccary@yahoo.com>
2018-12-17 20:48:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,13 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-data.collect do |attributes, descriptions|
-end
-puts new_hash
+data.each do |attribute, descriptions|
+  descriptions.each do |description, pigeons|
+    pigeons.each do |pigeon|
+      new_hash[pigeon] ||= {}
+      new_hash[pigeon][attribute] ||=[]
+      new_hash[pigeon][attribute] << option.to_s
     end
+  end
+end
+new_hash
 end
